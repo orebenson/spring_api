6f72df9cc566e488b2debc72f63684541ceec2e3
osbson <orebenson@hotmail.com>
2023-12-21 23:48:32 +0000

    Added TokenCipher class

--- src/main/java/com/example/myapi/jwt/service/TokenServiceImpl.java
+++ src/main/java/com/example/myapi/jwt/service/TokenServiceImpl.java
@@ -1,5 +1,44 @@
 package com.example.myapi.jwt.service;
+
+import com.example.myapi.jwt.model.TokenRevokerRepository;
+import com.example.myapi.jwt.util.TokenCipher;
+import com.google.crypto.tink.CleartextKeysetHandle;
+import com.google.crypto.tink.JsonKeysetReader;
+import com.google.crypto.tink.KeysetHandle;
+
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/*
+JWT management.
+jwt in the form of [Base64(HEADER)].[Base64(PAYLOAD)].[Base64(SIGNATURE)]
+ */
 public class TokenServiceImpl implements TokenService {
+
+    // Accessor for HMAC key - Block serialization and storage as String in JVM memory
+    private transient byte[] keyHMAC = null;
+
+    // Accessor for Ciphering key - Block serialization
+    private transient KeysetHandle keyCiphering = null;
+
+    //Accessor for Issuer ID - Block serialization
+    private transient String issuerID = null;
+
+    private TokenCipher tokenCipher;
+    private TokenRevokerRepository tokenRevokerRepository;
+
+    public TokenServiceImpl() throws Exception {
+        //Load keys from configuration text/json files in order to avoid to store keys as String in JVM memory
+        this.keyHMAC = Files.readAllBytes(Paths.get("src", "main", "conf", "key-hmac.txt"));
+        this.keyCiphering = CleartextKeysetHandle.read(JsonKeysetReader.withInputStream(new FileInputStream("src/main/conf/key-ciphering.json")));
+
+        //Load issuer ID from configuration text file
+        this.issuerID = Files.readAllLines(Paths.get("src", "main", "conf", "issuer-id.txt")).get(0);
+    }
+
+
+
     @Override
     public String getToken(String username) {
         return null;
--- src/main/java/com/example/myapi/jwt/util/TokenCipher.java
+++ src/main/java/com/example/myapi/jwt/util/TokenCipher.java
@@ -1,5 +1,64 @@
 package com.example.myapi.jwt.util;
 
+import com.google.crypto.tink.Aead;
+import com.google.crypto.tink.KeysetHandle;
+import com.google.crypto.tink.aead.AeadConfig;
+import com.google.crypto.tink.aead.AeadFactory;
+
+import javax.xml.bind.DatatypeConverter;
+
 public class TokenCipher {
+    /*
+    Ciphering using AES-GCM
+    Cipher and decipher token using google tink library for best practises
+    Aead hides all ciphering logic for security
+     */
+    public TokenCipher() throws Exception {
+        AeadConfig.register(); // registers key manager in 'Registry', a global storage of key managers
+    }
+
+    /*
+    Cipher a JWT
+    - takes jwt to cipher, pointer to keyset handle
+    - returns ciphered token in hex
+     */
+    public String cipherToken(String jwt, KeysetHandle keysetHandle) throws Exception {
+        if (jwt == null || jwt.isEmpty() || keysetHandle == null) {
+            throw new IllegalArgumentException("Both parameters must be present");
+        }
+
+        // get primitive aead
+        Aead aead = keysetHandle.getPrimitive(Aead.class);
+
+        // cipher the token
+        byte[] cipheredToken = aead.encrypt(jwt.getBytes(), null);
+
+        //convert token to hex
+        return DatatypeConverter.printHexBinary(cipheredToken);
+    }
+
+    /*
+    Decipher a jwt
+    - takes jwt in hex
+    - takes keyset handle
+    - returne cleartext token
+     */
+    public String decipherToken(String jwtInHex, KeysetHandle keysetHandle) throws Exception {
+        if (jwtInHex == null || jwtInHex.isEmpty() || keysetHandle == null) {
+            throw new IllegalArgumentException("Both parameters must be present");
+        }
+
+        // convert token back to byte
+        byte[] cipheredToken = DatatypeConverter.parseHexBinary(jwtInHex);
+
+        // get primitive
+        Aead aead = keysetHandle.getPrimitive(Aead.class);
+
+        // decipher token using primitive
+        byte[] decipheredToken = aead.decrypt(cipheredToken, null);
+
+        return new String(decipheredToken);
+    }
+
 
 }
